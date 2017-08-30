e2e27f5ef5ca7df36321fbb7fe15a28c318553ab
Will Hankinson <learnyourabcs@gmail.com>
2017-08-30 17:17:35 -0400

    added a shower thought

--- UnityUITest/Assets/SuperUITools/Scripts/SuperLabel.cs
+++ UnityUITest/Assets/SuperUITools/Scripts/SuperLabel.cs
@@ -11,6 +11,13 @@ using UnityEngine.UI;
     and then either adapt the source Text or build a custom Graphic to support
     bitmap fonts (probably with way less bells & whistles)
  */
+ /*
+    Showerthought: one possible intermediate to full on bitmap fonts would be to
+    add a flag to DAMetaNode for moving all labels to top. When moving a label 
+    to the top, replace it with a tracking GameObject and every frame synch
+    the label's position/rotation/scale to the tracking object in the event
+    that the container with the tracking object in it has moved.
+  */
 public class SuperLabel : SuperNode 
 {
     
